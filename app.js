ef7d66c0ef7faf5aef61e89df59dcdf5559dfb9d
abdulw786 <47833187+abdulw786@users.noreply.github.com>
2021-02-11 23:41:34 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
   res.send('Nodejs for github test');
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8000);
 
 module.exports = app;
